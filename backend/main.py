1bf9e60b914085dbad7b42c6f2f9104a9762f9f4
Nguyen Dao Nam Hai <namhai23092005@gmail.com>
2026-03-29 10:50:58 +0700

    xây nền để làm frontend

--- backend/main.py
+++ backend/main.py
@@ -1,23 +1,44 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, WebSocket
+from fastapi.middleware.cors import CORSMiddleware
+import paho.mqtt.client as mqtt
+import asyncio
+import json
 
-#Khởi tạo FastAPI
-app = FastAPI(
-    title="IV Drip AI Hub - API Server", 
-    description="Hệ thống quản lý truyền dịch thông minh",
-    version="1.0"
+app = FastAPI(title="IV Drip AI Hub - Core API")
+
+#Cấu hình CROCS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], # Cho phép mọi cổng
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
-#Tạo một Endpoint kiểm tra Server
-@app.get("/")
-def read_root():
-    return {"message": "Trạm chỉ huy Backend đã sẵn sàng!", "status": "Online"}
+#LOGIC LẮNG NGHE MQTT
+latest_data = {"device": "Đang chờ kết nối...", "current": 0.0, "target": 0.0}
 
-#Tạo một Endpoint để lấy thông tin thiết bị 
-@app.get("/api/devices/{device_id}")
-def get_device_status(device_id: str):
-    return {
-        "device_id": device_id,
-        "target_rate": 45.0,
-        "current_rate": 45.2,
-        "status": "active"
-    }
+def on_message(client, userdata, msg):
+    global latest_data
+    try:
+        payload = json.loads(msg.payload.decode('utf-8'))
+        latest_data = payload 
+    except:
+        pass
+
+mqtt_client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
+mqtt_client.on_message = on_message
+mqtt_client.connect("127.0.0.1", 1883, 60)
+mqtt_client.subscribe("hospital/telemetry/#")
+mqtt_client.loop_start()
+
+#ĐƯỜNG ỐNG WEBSOCKET CHO FRONTEND
+@app.websocket("/ws/telemetry")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    try:
+        while True:
+            await websocket.send_json(latest_data)
+            await asyncio.sleep(0.5)
+    except Exception as e:
+        print("Frontend đã ngắt kết nối")
