2865746247f7cc5f2ee54ab1d8a58a8fd55df60d
Nguyen Dao Nam Hai <namhai23092005@gmail.com>
2026-03-30 09:22:01 +0700

    refactor code

--- ai_and_workers/mqtt_worker/main.py
+++ ai_and_workers/mqtt_worker/main.py
@@ -3,7 +3,6 @@ import paho.mqtt.client as mqtt
 from influxdb_client import InfluxDBClient, Point
 from influxdb_client.client.write_api import SYNCHRONOUS
 
-# --- CẤU HÌNH INFLUXDB (Khớp 100% với docker-compose của Nam Hải) ---
 INFLUX_URL = "http://localhost:8087"
 INFLUX_TOKEN = "aEyoAt8SbmDUhu_x6G2TEpsW2ecxDJJzrHBNBJXsfD8Blnx1hVlMibpjU02MFKD1bSg_KQLt3JK9AI3vSOXlAw=="
 INFLUX_ORG = "soict"
@@ -12,7 +11,6 @@ INFLUX_BUCKET = "telemetry_bucket"
 client_influx = InfluxDBClient(url=INFLUX_URL, token=INFLUX_TOKEN, org=INFLUX_ORG)
 write_api = client_influx.write_api(write_options=SYNCHRONOUS)
 
-# --- CẤU HÌNH MQTT ---
 MQTT_BROKER = "127.0.0.1"
 MQTT_PORT = 1883
 MQTT_TOPIC = "hospital/telemetry/#"
@@ -23,14 +21,12 @@ def on_connect(client, userdata, flags, rc):
 
 def on_message(client, userdata, msg):
     try:
-        # Bóc tách JSON
         payload = json.loads(msg.payload.decode("utf-8"))
         device_id = payload.get("device", "Unknown")
         current_rate = float(payload.get("current", 0))
         target_rate = float(payload.get("target", 0))
         valve_angle = float(payload.get("angle", 0))
 
-        # Đóng gói chuẩn InfluxDB
         point = (
             Point("iv_telemetry")
             .tag("device_id", device_id)
@@ -38,15 +34,13 @@ def on_message(client, userdata, msg):
             .field("target_rate", target_rate)
             .field("valve_angle", valve_angle)
         )
-        
-        # Ghi vào kho telemetry_bucket
+
         write_api.write(bucket=INFLUX_BUCKET, org=INFLUX_ORG, record=point)
         print(f"💾 LƯU INFLUXDB -> [{device_id}] Current: {current_rate} | Valve: {valve_angle}")
 
     except Exception as e:
         print(f"❌ Lỗi: {e}")
 
-# Khởi động Worker
 mqtt_client = mqtt.Client()
 mqtt_client.on_connect = on_connect
 mqtt_client.on_message = on_message
