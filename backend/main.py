01f3826edb828941ab62a457610253e3269687a2
Nguyen Dao Nam Hai <namhai23092005@gmail.com>
2026-03-31 22:35:15 +0700

    Dựng trạm phát websocket từ server

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,29 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 from fastapi.middleware.cors import CORSMiddleware
 from contextlib import asynccontextmanager
+from typing import List
 from app.api.routes import router
 from app.services.mqtt_service import start_mqtt
+
+class ConnectionManager:
+    def __init__(self):
+        self.active_connections: List[WebSocket] = []
+
+    async def connect(self, websocket: WebSocket):
+        await websocket.accept()
+        self.active_connections.append(websocket)
+        print(f"🔗 Có thiết bị React mới kết nối! Tổng số: {len(self.active_connections)}")
+
+    def disconnect(self, websocket: WebSocket):
+        self.active_connections.remove(websocket)
+        print("❌ Một tab React vừa đóng kết nối.")
+
+    async def broadcast(self, message: str):
+        # Có tin báo động là loa lên cho tất cả các Tab React biết
+        for connection in self.active_connections:
+            await connection.send_text(message)
+
+manager = ConnectionManager()
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -23,6 +44,18 @@ app.add_middleware(
 )
 
 app.include_router(router)
+
+@app.websocket("/ws/telemetry")
+async def websocket_endpoint(websocket: WebSocket):
+    await manager.connect(websocket)
+    try:
+        while True:
+            data = await websocket.receive_text()
+
+            await manager.broadcast(data)
+            
+    except WebSocketDisconnect:
+        manager.disconnect(websocket)
 
 if __name__ == "__main__":
     import uvicorn
